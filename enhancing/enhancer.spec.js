d947a72a6bb04c11a61b4d1ec2c7c60b4b251276
Heather Beckman <heatherbeckman85@gmail.com>
2021-01-19 19:12:45 -0700

    initial commit, wrote repair function and test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,18 @@
+const { TestScheduler } = require('jest');
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe("Enhancer tests", () => {
+    it("repairs", () => {
+        expect(enhancer.repair({
+            name: "Item",
+            durability: 10,
+            enhancement: 5
+        }))
+            .toEqual({
+                name: "Item",
+                durability: 100,
+                enhancement: 5
+            })
+    })
+})
